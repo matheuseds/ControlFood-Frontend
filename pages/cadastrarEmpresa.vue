0b33e87879cbe952f994159680a87436c6da16bf
Nanda <mariafab@unipam.edu.br>
2022-03-27 17:52:43 -0300

    Tela de Cadastro de Empresas

---
+++ pages/cadastrarEmpresa.vue
@@ -0,0 +1,145 @@
+<template>
+  <main>
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Control Food</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarsExample02"
+        aria-controls="navbarsExample02"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample02">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#"
+              >Cadastrar Empresa <span class="sr-only">(current)</span></a
+            >
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#"
+              >Cadastrar Colaborador <span class="sr-only">(current)</span></a
+            >
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#"
+              >Relatório <span class="sr-only">(current)</span></a
+            >
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <div class="card">
+            <div class="card-body">
+              <h1>Cadastrar Empresa</h1>
+              <form id="form-contato">
+                <div class="form-group">
+                  <label for="nome">CNPJ:</label>
+                  <input
+                    class="form-control"
+                    type="text"
+                    name="nome"
+                    id="cnpj"
+                    placeholder="CNPJ"
+                    required 
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="email">Nome:</label>
+                  <input
+                    class="form-control"
+                    type="text"
+                    name="nome"
+                    id="nomeEmpresa"
+                    placeholder="Nome"
+                    required 
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="email">E-mail:</label>
+                  <input
+                    class="form-control"
+                    type="email"
+                    name="email"
+                    id="emailEmpresa"
+                    placeholder="E-mail"
+                    required 
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="email">Contato do Responsável:</label>
+                  <input
+                    class="form-control"
+                    type="text"
+                    name="nome"
+                    id="contatoEmpresa"
+                    placeholder="Contato do Responsável"
+                    required 
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="inputAddress">Endereço</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="endereco"
+                    placeholder="Rua dos Bobos, nº 0"
+                    required 
+                  />
+                </div>
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="inputCity">Cidade</label>
+                    <input type="text" class="form-control" id="cidade" required />
+                  </div>
+                  <div class="form-group col-md-3">
+                    <label for="inputEstado">Estado</label>
+                    <input type="text" class="form-control" id="estado" required  />
+                  </div>
+                  <div class="form-group col-md-4">
+                    <label for="inputCEP">CEP</label>
+                    <input type="text" class="form-control" id="cep" required />
+                  </div>
+                </div>
+
+                <div class="form-group text-center">
+                  <button type="submit" class="btn btn-primary">ENVIAR</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    email: '',
+    password: '',
+  }),
+
+  methods: {
+    login() {
+      alert('login')
+    },
+  },
+}
+</script>
+
+<style>
+.row {
+  margin-top: 5%;
+  justify-content: center;
+}
+</style>
