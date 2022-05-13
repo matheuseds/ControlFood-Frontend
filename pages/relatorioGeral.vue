a9033de70aeb74707fcbd31e250816de82779007
Nanda <mariafab@unipam.edu.br>
2022-05-13 15:26:03 -0300

    Update relatorioGeral.vue

--- pages/relatorioGeral.vue
+++ pages/relatorioGeral.vue
@@ -50,18 +50,33 @@
       <div>
         <button type="button" class="btn btn-dark botao">Imprimir</button>
         <div class="row">
-          <b-dropdown
-            id="dropdown-1"
-            text="Empresas"
-            class="m-md-2 dropdown"
-            variant="dark"
-          >
-            <b-dropdown-item>Empresa 1</b-dropdown-item>
-            <b-dropdown-item>Empresa 2</b-dropdown-item>
-            <b-dropdown-item>Empresa 3</b-dropdown-item>
-          </b-dropdown>
         </div>
       </div>
+    </div>
+      <div class="container input-group pesquisa">
+      <div class="pesquisa">
+        <input
+          type="search"
+          id="form1"
+          class="form-control"
+          v-model="pesquisa"
+          placeholder="Pesquisar Empresa"
+        />
+      </div>
+      <button type="button" class="btn btn-dark">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-search"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
+          />
+        </svg>
+      </button>
     </div>
 
     <div class="container">
