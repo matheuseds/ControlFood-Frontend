f25408bcf009f814665a8f9db94996a30aebe972
JoaoFlavioRodrigues <64814225+Joaorodrigues7132@users.noreply.github.com>
2022-05-15 19:04:07 -0300

    alteração icon
    ;;;

--- pages/dashboardSite.vue
+++ pages/dashboardSite.vue
@@ -135,11 +135,15 @@
             <tr>
               <td>{{ colaborador.id }}</td>
               <td>{{ colaborador.nome }}</td>
-              <td>{{ colaborador.cpf }}</td>
+              <td class=" alinhar">{{ colaborador.cpf }}</td>
               <td>{{ colaborador.nome_empresa }}</td>
-              <td><button style="background-color:green"><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-  <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
-</svg></button></td>
+              <td><b-button
+                style="background-color: #16a085"
+                v-b-modal.modal-1
+                @click="id = empresa.id"
+              >
+                <b-icon-check></b-icon-check>
+              </b-button></td>
             </tr>
           </tbody>
         </table>
