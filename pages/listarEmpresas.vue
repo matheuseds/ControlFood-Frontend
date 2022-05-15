b2729ecef1dfb344d6bd7ee658260bfe7f2926cb
JoaoFlavioRodrigues <64814225+Joaorodrigues7132@users.noreply.github.com>
2022-05-15 14:15:50 -0300

    111
    1111

--- pages/listarEmpresas.vue
+++ pages/listarEmpresas.vue
@@ -81,7 +81,8 @@
             <th>CNPJ</th>
             <th>Empresa</th>
             <th>Contato do Responsável</th>
-            <th>E-mail</th>
+            <th>E-mail</th> 
+            <th></th>
           </tr>
         </thead>
 
@@ -92,6 +93,9 @@
             <td>{{ empresa.nomeEmpresa }}</td>
             <td>{{ empresa.contatoResponsavel }}</td>
             <td>{{ empresa.email }}</td>
+            <td><button style="background-color:red"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+</svg></button></td>
           </tr>
         </tbody>
       </table>
--- pages/relatorioGeral.vue
+++ pages/relatorioGeral.vue
@@ -77,6 +77,28 @@
           />
         </svg>
       </button>
+      <button onclick="window.location.reload()"
+      
+        type="button" class="btn btn-dark"> 
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-search"
+          viewBox="0 0 16 16"
+         
+          >
+          <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+  <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"
+  />
+        </svg>
+   
+
+
+
+        
+      </button>
     </div>
 
     <div class="container">
@@ -106,6 +128,7 @@
 </template>
 
 <script>
+
 export default {
   data: () => ({
     relatorios: [
@@ -122,10 +145,14 @@ export default {
         empresa: 'Empresa',
         CPF: '303.810.860-03',
         refeicoes: '20',
+        
       },
-    ],
+     
+    ], 
   }),
+ 
 }
+
 </script>
 
 <style>
